ccc7493273aac207429aad20635c4110d4ed6bb6
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-14 04:38:10 +0100

    Changing variable names to avoid conflicts And changes to the list of commands and folders to ignore

--- src/Environments/Laravel/V_Pack1/routes_commands.php
+++ src/Environments/Laravel/V_Pack1/routes_commands.php
@@ -1,7 +1,8 @@
 <?php
-foreach (scandir(__DIR__ . '\Commands') as $key => $value) {
-    $c ='Compio\Environments\Laravel\V_Pack1\Commands\\' . ucfirst(pathinfo($value)['filename']);
-    if($value != '.' && $value != '..' && class_exists($c)){
-        $this->commands[] = $c;
+$exceptions_e82ye2yFFTDYQ272 = ['.', '..', 'Base.php'];
+foreach (scandir(__DIR__ . '\Commands') as $value_29e2hUHSYUQGY28y2HUH) {
+    $c_93h383hIHHejhhuwee2832 ='Compio\Environments\Laravel\V_Pack1\Commands\\' . ucfirst(pathinfo($value_29e2hUHSYUQGY28y2HUH)['filename']);
+    if(!in_array($value_29e2hUHSYUQGY28y2HUH, $exceptions_e82ye2yFFTDYQ272) && class_exists($c_93h383hIHHejhhuwee2832)){
+        $this->commands[] = $c_93h383hIHHejhhuwee2832;
     }
 }
