c8010ff57bd892ecd2050be5427b269876edd899
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-16 20:47:50 +0100

    Create : Creation du model de configuration de la librairie

---
+++ src/Environments/Laravel/V_Pack1/resources/config.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Storage Paths
+    |--------------------------------------------------------------------------
+    |
+    | Most templating systems load templates from disk. Here you may specify
+    | an array of paths that should be checked for your views. Of course
+    | the usual Laravel view path has already been registered for you.
+    |
+    */
+    'component' => [
+        'paths' => [
+            'class' => getcwd() . '\app\View\Components',
+            'render' => getcwd() . '\resources\views\component',
+            'assets' => [
+                'css' => getcwd() . '\public\css\components',
+                'js' => getcwd() . '\public\css\components',
+            ]
+        ],
+        'replace_component_exist' => true,
+        'models' => null,
+        'components' => [
+            [
+                'config' => [
+                    'replace_component_exist' => true,
+                    'models' => null,
+                ],
+                'name' => 'Modals/Roy',
+                'args' => [
+                    'string:array:title' => 'My Modal',
+                    'string:description' => 'Lorem ipsum dolor sit amet',
+                ]
+            ],
+            [
+                'name' => 'Menu/Horiz',
+                'args' => [
+                    'array:datas' => '#!require'
+                ]
+            ]
+        ]
+    ]
+    'layout' => []
+];
