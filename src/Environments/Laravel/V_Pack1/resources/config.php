6c895fd594533fe56ea1a8c99a4a645d3c0a81ae
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-17 08:54:33 +0100

    Update | Add : Restructured `compio` config model

--- src/Environments/Laravel/V_Pack1/resources/config.php
+++ src/Environments/Laravel/V_Pack1/resources/config.php
@@ -13,16 +13,18 @@ return [
     |
     */
     'component' => [
-        'paths' => [
-            'class' => getcwd() . '\app\View\Components',
-            'render' => getcwd() . '\resources\views\component',
-            'assets' => [
-                'css' => getcwd() . '\public\css\components',
-                'js' => getcwd() . '\public\css\components',
-            ]
+        'config' => [
+            'paths' => [
+                'class' => getcwd() . '\app\View\Components',
+                'render' => getcwd() . '\resources\views\component',
+                'assets' => [
+                    'css' => getcwd() . '\public\css\components',
+                    'js' => getcwd() . '\public\css\components',
+                ]
+            ],
+            'replace_component_exist' => true,
+            'models' => null,
         ],
-        'replace_component_exist' => true,
-        'models' => null,
         'components' => [
             [
                 'config' => [
