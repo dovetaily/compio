01e7a8dfdad6729ff77334b67b46c326b08ea1dd
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-18 18:26:54 +0100

    Update | Remove : Removal of commented sections

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -7,28 +7,6 @@ use Compio\Environments\Laravel\Listen as L_Laravel;
 
 class ComposerScripts
 {
-    // /**
-    //  * Handle the post-install Composer event.
-    //  *
-    //  * @param  \Composer\Script\Event  $event
-    //  * @return void
-    //  */
-    // public static function postInstall(Event $event){
-    // }
-
-    // /**
-    //  * Handle the post-update Composer event.
-    //  *
-    //  * @param  \Composer\Script\Event  $event
-    //  * @return void
-    //  */
-    // public static function postUpdate(Event $event)
-    // {
-    //     require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
-
-    //     static::clearCompiled();
-    // }
-
     /**
      * Handle the post-autoload-dump Composer event.
      *
@@ -41,9 +19,4 @@ class ComposerScripts
             L_Laravel::getInstance();
         }
     }
-    public static function hh()
-    {
-        file_put_contents(__DIR__ . '\helpers.php', 'data');
-    }
 }
-// var_dump(app()->runningInConsole()); // verifie si nous sommes dans la console
