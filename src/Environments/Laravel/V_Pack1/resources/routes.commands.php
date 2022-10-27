1593c722ac687e1ea0e147b17afad2f36ba60d2a
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-27 09:36:09 +0100

    Update : Rename `src/Environments/Laravel/V_Pack1` and `src/TemplateEngines/Blade/V_Pack1` folders to `src/.../V_sup_eq_5_5`

--- src/Environments/Laravel/V_Pack1/resources/routes.commands.php
+++
@@ -1,8 +0,0 @@
-<?php
-$exceptions_e82ye2yFFTDYQ272 = ['.', '..', 'Base.php'];
-foreach (scandir(dirname(__DIR__) . '\Commands') as $value_29e2hUHSYUQGY28y2HUH) {
-    $c_93h383hIHHejhhuwee2832 ='Compio\Environments\Laravel\V_Pack1\Commands\\' . ucfirst(pathinfo($value_29e2hUHSYUQGY28y2HUH)['filename']);
-    if(!in_array($value_29e2hUHSYUQGY28y2HUH, $exceptions_e82ye2yFFTDYQ272) && class_exists($c_93h383hIHHejhhuwee2832)){
-        $this->commands[] = $c_93h383hIHHejhhuwee2832;
-    }
-}
