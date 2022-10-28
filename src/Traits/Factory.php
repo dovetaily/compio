2e5db9ac3568c2bc676685c01ae7db7c288ad42c
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-28 01:31:12 +0100

    Update : Update of the `getFactoryNameSpace` and `__callStatic` methods and restructuring of the code

--- src/Traits/Factory.php
+++ src/Traits/Factory.php
@@ -1,22 +1,40 @@
 <?php
+
 namespace Compio\Traits;
-/**
- * 
- */
 
 trait Factory {
 
+	/**
+	 * Get factory namespace of class. 
+	 *
+	 * @return string
+	 * 
+	 * @throws \RuntimeException
+	 */
 	protected static function getFactoryNameSpace(){
 
-		throw new Exception("Error Processing Request", 1);
+		throw new RuntimeException('Factory does not implement getFactoryNameSpace method.');
 
 	}
 
+
+	/**
+     * Handle dynamic, calls to the object and
+     * get instance of a factory class. 
+	 *
+	 * @param  string  $method
+	 * @param  mixed   $args
+	 * @return object
+	 * 
+	 * @throws \RuntimeException
+	 */
 	public static function __callStatic($method, $args){
 
 		$c = static::getFactoryNameSpace() . '\\' . ucfirst($method);
 
 		if(class_exists($c)) return method_exists($c, 'getInstance') ? $c::getInstance(...$args) : new $c(...$args);
+
+		throw new RuntimeException('The factory class `' . $c . '` is not found !.');
 
 	}
 
