1b8133ba06e6121bc1527e5ceeec3a0294dd41b0
gedeontimothy <gedeonbateko3@gmail.com>
2022-10-28 01:17:45 +0100

    Update : Code restructuring and adding and changing method descriptions

--- src/Traits/Singleton.php
+++ src/Traits/Singleton.php
@@ -1,13 +1,22 @@
 <?php
+
 namespace Compio\Traits;
-/**
- * 
- */
 
 trait Singleton {
 
+	/**
+	 * The instance of the class. 
+	 *
+	 * @return void
+	 */
 	protected static $instance;
 
+	/**
+	 * Get instance of a class. 
+	 *
+	 * @param  array  $args
+	 * @return void
+	 */
 	public static function getInstance(...$args){
 
 		$c = static::class;
