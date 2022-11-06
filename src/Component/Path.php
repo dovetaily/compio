d60fa0bbe82c822031e72910412b89ee8e3ab613
gedeontimothy <gedeonbateko3@gmail.com>
2022-11-06 06:27:23 +0100

    Add | Update : Update of the `Compio\Component\Path::init` method and addition of the `Compio\Component\Path::convert_case` method used to convert a text according to the criteria of a component

--- src/Component/Path.php
+++ src/Component/Path.php
@@ -47,7 +47,33 @@ class Path {
 
 				}
 
-				$this->addPath($key, $value['path'], (array_key_exists('file_extension', $value) ? $value['file_extension'] : "php"));
+				$ext = array_key_exists('file_extension', $value) && (is_string($value['file_extension']) || (is_array($value['file_extension']) && !empty($value['file_extension']))) 
+					? $value['file_extension'] 
+					: "php"
+				;
+				$ext = is_array($ext) 
+					? (is_string($v = end($ext))
+						? trim($v)
+						: 'php'
+					)
+					: trim($ext)
+				;
+
+				$convert_case = array_key_exists('convert_case', $value) 
+					? (is_array($v = $value['convert_case']) && !empty($v) && is_string($v = end($v)) && !empty(trim($v))
+						? trim($v)
+						: (is_string($value['convert_case']) && !empty(trim($value['convert_case']))
+							? trim($value['convert_case'])
+							: (is_callable($value['convert_case'])
+								? $value['convert_case']
+								: 'lower'
+							)
+						)
+					)
+					: 'lower'
+				;
+
+				$this->addPath($key, $value['path'], $ext, $convert_case);
 
 				$this->mergeWithTemplate('path', $key, $this->paths[$key]);
 
@@ -72,30 +98,73 @@ class Path {
 	/**
 	 * Add path.
 	 *
-	 * @param  string|null  $key
+	 * @param  string             $template
+	 * @param  array|string|null  $value
+	 * @param  string             $ext
+	 * @param  string|callable    $convert_case
 	 * @return Compio\Component\Path
 	 */
-	public function addPath(string $key, array|string|null $value, bool|array|string|null $ext){
+	public function addPath(string $template, array|string|null $value, string $ext, string|callable $convert_case = 'lower'){
 
 		$value = is_array($value) ? $value : [$value];
 
-		$ext = is_array($ext) ? end($ext) : $ext;
-
-		$this->paths[$key] = [];
+		$this->paths[$template] = [];
 
 		foreach ($value as $k => $val){
 
-			$f = $val . '\\' . $this->name()->getClassName() . ".$ext";
+			$short = $this->convert_case($this->name()->getClassName(), $convert_case);
+
+			$f = $val . '\\' . $short . ".$ext";
 
 			$vv = pathinfo($f);
 			$vv['file'] = $vv['dirname'] . '\\' . $vv['basename'];
+			$vv['short'] = $short;
 
-			if(empty($this->paths[$key]) || !in_array($vv, $this->paths[$key]))
-				$this->paths[$key][$k] = $vv;
+			if(empty($this->paths[$template]) || !in_array($vv, $this->paths[$template]))
+				$this->paths[$template][$k] = $vv;
 
 		}
 
 		return $this;
+
+	}
+	/**
+	 * Get converted text according to a component's criteria
+	 * 
+	 * @param string          $value
+	 * @param string|callable  $type
+	 * @return string
+	*/
+	public function convert_case($value, $type){
+
+		if (!is_string($type) && is_callable($type)) {
+
+			return is_string($val = $type(...[$value])) && !empty(trim($val)) && \Compio\Component\Name::nameIsCheck($val, ['name'])
+				? (((bool) preg_match('/' . preg_quote($value) . '/i', $val)) == true
+					? $val
+					: strtolower($value)
+				)
+				: strtolower($value)
+			;
+
+		}
+
+		return is_string($type) 
+			? ($type == 'default' || $type == 'd'
+				? $value
+				: ($type == 'upper' || $type == 'u'
+					? strtoupper($value)
+					: ($type == 'ucfirst' || $type == 'uc_first' || $type == 'uf'
+						? preg_replace_callback('/\\\([^ ])/i', function($_1){return strtoupper(current($_1));}, ucfirst($value))
+						: ($type == 'lcfirst' || $type == 'lc_first' || $type == 'lf'
+							? preg_replace_callback('/\\\([^ ])/i', function($_1){return strtolower(current($_1));}, lcfirst($value))
+							: strtolower($value)
+						)
+					)
+				)
+			)
+			: strtolower($value)
+		;
 
 	}
 
