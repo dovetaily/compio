edd4ea7cf55e8fab0eaf994ecac42779d247ffb6
gedeontimothy <gedeonbateko3@gmail.com>
2022-11-09 04:03:38 +0100

    Add | Create | Update : Creation of the class `Compio\Traits\Compliant` used to check values conforming to a structure and update of the class `Compio\Component\Template` by adding a method `getTemplateStructure` and an update of its method ` templateToGenerate`

--- src/Component/Template.php
+++ src/Component/Template.php
@@ -3,6 +3,7 @@
 namespace Compio\Component;
 
 use Compio\Traits\ComponentCaller;
+use Compio\Traits\Compliant;
 
 class Template {
 
@@ -34,20 +35,110 @@ class Template {
 
 		$t = [];
 
+		$error = [];
+
 		foreach ($datas as $key => $value) {
 
-			if(!in_array($key, $templates)) $datas[$key]['generate'] = false;
-			else{
+			$datas[$key]['generate'] = false;
+
+			$c = null;
+
+			if(in_array($key, $templates) && ($c = Compliant::is_compliant($key, $value, self::getTemplateStructure())) === true){
+
 				$datas[$key]['generate'] = true;
 
 				$t[$key] = $datas[$key];
+
 			}
+			elseif($c !== null) $error[$key] = $c;
 
 		}
 
 		$this->config()->setMerge($datas, 'template');
 
 		$this->templates = $t;
+
+		return empty($error) ? true : $error;
+
+	}
+
+	/**
+	 * Get template keys structure.
+	 *
+	 * @param  array  $templates
+	 * @return void
+	 */
+
+	public static function getTemplateStructure(){
+
+		return [
+			'path' => [
+				'require' => true,
+				'verif' => 'is_string',
+				'last' => false,
+				'empty' => false
+			],
+			'template_file' => [
+				'require' => true,
+				'verif' => function($value){
+
+					return is_string($value) && is_readable($value)
+						? true
+						: (!is_string($value) 
+							? 'La clé `template_file` n\'est pas une chaîne de caractère !'
+							: "Le fichier `$value` de votre clé `template_file` n'a pas été trouvé ou ne peut être lu en raison des autorisations de lecture du fichier !"
+						)
+					;
+
+				},
+				'last' => true,
+				'empty' => false
+			],
+			'generate' => [
+				'require' => false,
+				'verif' => 'is_bool',
+				'last' => true,
+				'empty' => true
+			],
+			'convert_case' => [
+				'require' => false,
+				'verif' => function($value){
+
+					return is_string($value) || is_callable($value)
+						? true
+						: 'La clé `convert_case` n\'est pas une chaîne de caractère ou une fonction de rappel !'
+					;
+
+				},
+				'last' => true,
+				'empty' => false
+			],
+			'file_extension' => [
+				'require' => false,
+				'verif' => 'is_string',
+				'last' => true,
+				'empty' => false
+			],
+			'short_path' => [
+				'require' => false,
+				'verif' => 'is_string',
+				'last' => true,
+				'empty' => false
+			],
+			'keywords' => [
+				'require' => false,
+				'verif' => function($value){
+
+					return is_string($value) || is_callable($value) || is_null($value)
+						? true
+						: 'La(les) valeur(s) de la clé `keywords` n\'est(ne sont) pas conforme(s) !'
+					;
+
+				},
+				'last' => false,
+				'empty' => true
+			]
+		];
 
 	}
 
---
+++ src/Traits/Compliant.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Compio\Traits;
+
+trait Compliant {
+
+	/**
+	 * Check values is compliant
+	 *
+	 * @param  array  $templates
+	 * @return bool|array
+	 */
+	public static function is_compliant($template, $value, $keys_structure) : bool|array{
+
+		$error = [];
+
+		foreach ($keys_structure as $key => $val) {
+			$resp = null;
+			if(
+				(!array_key_exists($key, $value) && $val['require'] === true) 
+					||
+				array_key_exists($key, $value) 
+					&&
+				(
+					!(
+						array_key_exists($key, $value)
+							&&
+						(
+							$resp = self::is_conforming($value[$key], $key, $val['verif'], $val['last'], (
+								array_key_exists('empty',$val) 
+									? ((bool) $val['empty']) 
+									: false
+							))
+						) === true
+					)
+				)
+			) $error[$key] = $resp !== null ? $resp : "La clé `$key` n'existe pas !";
+		}
+		return empty($error) ? true : $error;
+
+	}
+
+	/**
+	 * Check values is conforming
+	 *
+	 * @param  mixed                 $datas
+	 * @param  string                $key
+	 * @param  string|callable|null  $verif
+	 * @param  bool                  $last
+	 * @param  bool                  $empty
+	 * @return string|bool
+	 */
+	private static function is_conforming($datas, string $key, string|callable|null $verif = null, bool $last = true, bool $empty = false) : string|bool{
+
+		$verif = is_string($verif) && function_exists('\\' . $verif) 
+			? $verif 
+			: (!is_string($verif) && is_callable($verif)
+				? $verif
+				: 'is_string'
+			);
+		$datas = is_array($datas) ? $datas : [$datas];
+		$resp = null;
+		if(($resp = (function($datas, $key, $fn, $last, $empty){
+			$datas = $last === true && count($datas) > 1 ? [end($datas)] : $datas;
+			foreach ($datas as $value){
+				if($empty === false && (empty($value) && $value !== false)) return count($datas) > 1
+					? "L'une des valeurs de la clé `$key` est vide !"
+					: "La clé `$key` est vide !"
+				;
+				elseif($empty === true && (empty($value) && $value !== false)) continue;
+				if(($resp = $fn(...[$value])) !== true) return $resp;
+			}
+
+			return true;
+
+		})($datas, $key, $verif, $last, $empty)) === true ){
+			return true;
+		}
+		else
+			return $resp === false
+				? "Verifiez la valeur de la clé `$key` car elle n'est pas conforme !"
+				: $resp
+			;
+
+	}
+
+}
